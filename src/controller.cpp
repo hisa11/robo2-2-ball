8ae1c68faf2f3267e996e60b4bfcd4572238a563
hisa11 <hsiahisa1120@outlook.jp>
2024-08-26 16:30:39 +0900

    modified:   platformio.ini 	modified:   src/controller.cpp 	modified:   src/main.cpp

--- src/controller.cpp
+++ src/controller.cpp
@@ -13,7 +13,7 @@ void processInput(char *output_buf)
         // 数字部分を読み取る
         leftJoystickX = atoi(dataPointer);
         // 数字を使って何かをする（ここでは単にPCに出力）
-        printf("Left Joystick X: %d\n", leftJoystickX);
+        // printf("Left Joystick X: %d\n", leftJoystickX);
     }
     else if (strncmp(output_buf, "L3_y:", 5) == 0) // "L3_y:"という文字列で始まるかどうかを確認します
     {
@@ -21,7 +21,7 @@ void processInput(char *output_buf)
         // 数字部分を読み取る
         leftJoystickY = atoi(dataPointer);
         // 数字を使って何かをする（ここでは単にPCに出力）
-        printf("Left Joystick Y: %d\n", leftJoystickY);
+        // printf("Left Joystick Y: %d\n", leftJoystickY);
     }
     else if (strncmp(output_buf, "R3_x:", 5) == 0) // "R3_x:"という文字列で始まるかどうかを確認します
     {
@@ -29,7 +29,7 @@ void processInput(char *output_buf)
         // 数字部分を読み取る
         rightJoystickX = atoi(dataPointer);
         // 数字を使って何かをする（ここでは単にPCに出力）
-        printf("Right Joystick X: %d\n", rightJoystickX);
+        // printf("Right Joystick X: %d\n", rightJoystickX);
         rightJoystickX = rightJoystickX / 5;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -30,13 +30,12 @@ uint8_t DATA[8] = {};
 // PID制御
 const float kp = 0.4;
 const float ki = 0.5;
-const float kd = 0.001;
+const float kd = 0.07/8;
 const float sampleTime = 0.01; // 20ms sample time
 
 // PID制御器のインスタンスを作成
 PID pidControllerRight(kp, ki, kd, sampleTime);
 PID pidControllerLeft(kp, ki, kd, sampleTime);
-
 
 // シリアル通信読み取りのプログラム
 void readUntilPipe(char *output_buf, int output_buf_size)
@@ -70,7 +69,6 @@ void readUntilPipe(char *output_buf, int output_buf_size)
     }
 }
 
-
 int main()
 {
     char output_buf[20]; // 出力用のバッファを作成します
@@ -80,9 +78,33 @@ int main()
     {
         readUntilPipe(output_buf, sizeof(output_buf)); // '|'が受け取られるまでデータを読み込みます
         processInput(output_buf);
+
+        if (strncmp(output_buf, "L1ON", 4) == 0) // "R3_x:"という文字列で始まるかどうかを確認します
+        {
+            penguin.pwm[0] = 10000;
+            penguin.pwm[1] = 10000;
+            penguin.pwm[2] = 10000;
+            penguin.pwm[3] = 10000;
+            // penguin.send();
+        }
+        else if (strncmp(output_buf, "L1OFF", 5) == 0 || strncmp(output_buf,"R1OFF",5)== 0 ) // "R3_x:"という文字列で始まるかどうかを確認します
+        {
+            penguin.pwm[0] = 0;
+            penguin.pwm[1] = 0;
+            penguin.pwm[2] = 0;
+            penguin.pwm[3] = 0;
+            // penguin.send();
+        }
+        else if (strncmp(output_buf, "R1ON", 4) == 0)
+        {
+            penguin.pwm[0] = -4000;
+            penguin.pwm[1] = -4000;
+            penguin.pwm[2] = -4000;
+            penguin.pwm[3] = -4000;
+        }
         
 
-        targetSpeedLeft = (leftJoystickY - rightJoystickX) * 2 / 3;
-        targetSpeedRight = (-leftJoystickY - rightJoystickX) * 2 / 3;
+        targetSpeedLeft = (leftJoystickY - rightJoystickX) * 7 / 12;
+        targetSpeedRight = (-leftJoystickY - rightJoystickX) * 7 / 12;
     }
 }
